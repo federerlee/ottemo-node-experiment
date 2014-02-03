dcac5fdd2f4052538ee07d5eb4540cf6123f50c5
James Vastbinder <james.vastbinder@gmail.com>
2014-02-02 23:11:19 -0800

    cleaned up gruntfile and fixed postgres lib

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,16 +123,16 @@ module.exports = function (grunt) {
 
   // Get path to core grunt dependencies from Sails
   var depsPath = grunt.option('gdsrc') || 'node_modules/sails/node_modules';
-  grunt.loadTasks(depsPath + '/grunt-contrib-clean/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-copy/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-concat/tasks');
+  grunt.loadTasks('node_modules' + '/grunt-contrib-clean/tasks');
+  grunt.loadTasks('node_modules' + '/grunt-contrib-copy/tasks');
+  grunt.loadTasks('node_modules' + '/grunt-contrib-concat/tasks');
   grunt.loadTasks(depsPath + '/grunt-sails-linker/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-jst/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-watch/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-uglify/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-cssmin/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-less/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-coffee/tasks');
+  grunt.loadTasks('node_modules' + '/grunt-contrib-jst/tasks');
+  grunt.loadTasks('node_modules' + '/grunt-contrib-watch/tasks');
+  grunt.loadTasks('node_modules' + '/grunt-contrib-uglify/tasks');
+  grunt.loadTasks('node_modules' + '/grunt-contrib-cssmin/tasks');
+  grunt.loadTasks('node_modules' + '/grunt-contrib-less/tasks');
+  grunt.loadTasks('node_modules' + '/grunt-contrib-coffee/tasks');
 
   // Project configuration.
   grunt.initConfig({
